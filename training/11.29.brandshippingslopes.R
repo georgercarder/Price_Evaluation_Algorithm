771ef81dab173edd7a26dc95d47f1b06a2277f06
George Carder <georgercarder@gmail.com>
2017-12-18 14:51:24 -0800

    upd *brandshippingslopes.R for p1 2

--- training/11.29.brandshippingslopes.R
+++ training/11.29.brandshippingslopes.R
@@ -4,6 +4,13 @@
 load("TRNMICRO")
 load("BRANDCAT")
 
+A<-TRNMICRO[,c(2,5,6)]
+B<-TRNMICRO[,c(3,4)]
+price<-TRNMICRO$price
+
+TRNMICRO<-cbind(B,A,price)
+rm(A,B,price)
+
 N<-nrow(BRANDCAT)
 NN=3
 
@@ -18,10 +25,10 @@ while(i<=N){
     while(j<=3){
         
 
-        if(length(A$price[A[,1+j]==1])==0|length(A$price[A[,1+j]==0])==0){
+        if(length(A$price[A[,2+j]==1])==0|length(A$price[A[,2+j]==0])==0){
             BRANDSHIPPINGSLOPES[i,j]=0
         }else{
-            BRANDSHIPPINGSLOPES[i,j]<-mean(A$price[A[,1+j]==1])-mean(A$price[A[,1+j]==0])
+            BRANDSHIPPINGSLOPES[i,j]<-mean(A$price[A[,2+j]==1])-mean(A$price[A[,2+j]==0])
 
         }
 
@@ -33,6 +40,7 @@ print(i)
 i=i+1
 }
 
+rm(TRNMICRO)
 save(BRANDSHIPPINGSLOPES,file="BRANDSHIPPINGSLOPES")
 rm(list=ls())
 
