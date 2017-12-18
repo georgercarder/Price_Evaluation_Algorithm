7f873d937a5164f71928214609e4e07b8b20a1d2
George Carder <georgercarder@gmail.com>
2017-12-18 12:17:40 -0800

    upd ncharnnamemeanSTD.R for p1, gives stdXYZ

--- training/ncharnnamemeanSTD.R
+++ training/ncharnnamemeanSTD.R
@@ -3,10 +3,19 @@
 #after ncharnnamemean.R used smoothZ.R to give values to NaNs.
 # now we scale wrt std and mean for trn$price
 
+load("trn")
+load("XYZ")
+
 m<-mean(trn$price)
 
 std<-sqrt((1/(nrow(trn)-1))*sum((trn$price-m)^2))
 
+rm(trn)
+
 stdXYZ<-XYZ
 
+rm(XYZ)
+
 stdXYZ[,3]=(XYZ[,3]-m)/std
+
+save(stdXYZ,file="stdXYZ")
