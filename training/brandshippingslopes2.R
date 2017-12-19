9cf5084fb73e9f60e3d4aa982d6c8d2d7c68ef44
George Carder <georgercarder@gmail.com>
2017-12-18 16:15:15 -0800

    wrote brandshippingslopes2.R to fill zero slopes

---
+++ training/brandshippingslopes2.R
@@ -0,0 +1,37 @@
+#!/bin/Rscript
+
+load("BRANDSHIPPINGSLOPES")
+load("TRNMICRO")
+load("BRANDCAT")
+
+A<-TRNMICRO[,c(2,5,6)]
+B<-TRNMICRO[,c(3,4)]
+price<-TRNMICRO$price
+
+TRNMICRO<-cbind(B,A,price)
+rm(A,B,price)
+
+N<-nrow(BRANDSHIPPINGSLOPES)
+BRANDSHIPPINGSLOPES2<-BRANDSHIPPINGSLOPES
+
+
+i=1
+while(i<=N){
+    
+    j=1
+    while(j<=3){
+        if(BRANDSHIPPINGSLOPES2[i,j]==0){
+            A<-TRNMICRO[TRNMICRO$cat1==BRANDCAT[i,2],]
+
+            BRANDSHIPPINGSLOPES2[i,j]<-mean(A$price[A[,2+j]==1])-mean(A$price[A[,2+j]==0])
+        }        
+
+    j=j+1
+    }
+
+print(i)
+i=i+1
+}
+
+save(BRANDCATSHIPPINGSLOPES2,file="BRANDCATSHIPPINGSLOPES2")
+rm(list=ls())
