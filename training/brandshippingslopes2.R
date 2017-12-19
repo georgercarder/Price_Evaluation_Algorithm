0249e9c62abdb50230893414d9a9a7549077333e
George Carder <georgercarder@gmail.com>
2017-12-18 21:01:50 -0800

    wrote slope2.R and ..shippingslopes2.R to fill 0 entries by extending to category

--- training/brandshippingslopes2.R
+++ training/brandshippingslopes2.R
@@ -20,10 +20,11 @@ while(i<=N){
     
     j=1
     while(j<=3){
-        if(BRANDSHIPPINGSLOPES2[i,j]==0){
+        if(BRANDSHIPPINGSLOPES[i,j]==0){
             A<-TRNMICRO[TRNMICRO$cat1==BRANDCAT[i,2],]
-
+            if(!(length(A$price[A[,2+j]==1])==0|length(A$price[A[,2+j]==0])==0)){
             BRANDSHIPPINGSLOPES2[i,j]<-mean(A$price[A[,2+j]==1])-mean(A$price[A[,2+j]==0])
+            }
         }        
 
     j=j+1
@@ -33,5 +34,5 @@ print(i)
 i=i+1
 }
 
-save(BRANDCATSHIPPINGSLOPES2,file="BRANDCATSHIPPINGSLOPES2")
+save(BRANDSHIPPINGSLOPES2,file="BRANDSHIPPINGSLOPES2")
 rm(list=ls())
