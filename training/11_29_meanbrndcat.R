b088e9e036e847c8e160ea48d1214748800b9999
George Carder <georgercarder@gmail.com>
2017-12-19 17:18:07 -0800

    upd *meanbrndcat.R to extend domain for 0 entries..

--- training/11_29_meanbrndcat.R
+++ training/11_29_meanbrndcat.R
@@ -13,7 +13,12 @@ i=1
 
 while(i<=N){
 
-    MEANBRANDCAT[i,3]=mean(TRNMICRO$price[TRNMICRO$brand1==MEANBRANDCAT[i,1]&TRNMICRO$cat1==MEANBRANDCAT[i,2]])
+    M<-mean(TRNMICRO$price[TRNMICRO$brand1==MEANBRANDCAT[i,1]&TRNMICRO$cat1==MEANBRANDCAT[i,2]])
+    if(M!=0){
+        MEANBRANDCAT[i,3]=M
+    }else{
+        MEANBRANDCAT[i,3]=mean(TRNMICRO$price[TRNMICRO$cat1==MEANBRANDCAT[i,2]])
+    }
 
 print(i)
 i=i+1
